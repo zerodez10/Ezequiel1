71c8c9e9072cad564950caceb9d9fd7a5d9ffb68
ezequiel <ezequielmantuanosousadossantos@gmail.com>
2025-06-04 10:16:54 -0300

    Add files via upload

---
+++ ezequiel.py
@@ -0,0 +1,13 @@
+palmeiras = 8
+santos = 4
+print(palmeiras + santos)
+print(palmeiras - santos)
+print(palmeiras * santos)
+print(palmeiras / santos)
+print(palmeiras % santos)
+nome= "ezequiel"
+print(nome)
+nome="ezequiel"
+print("meu nome é:", nome)
+idade=15
+print("minha idade é:",idade, "meu nome é:",nome)
